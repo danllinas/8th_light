2d3d693e975dbe2b0ee9eed737e1a20305099445
Daniel Llinas <danllinas@gmail.com>
2015-11-17 11:14:02 -0600

    Initial commit for code challenge.

---
+++ game_fix.rb
@@ -0,0 +1,127 @@
+class Game
+  attr_accessor :player_1, :player_2
+
+  def initialize
+    @board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+  end
+
+  def start_game
+    puts "Welcome to my Tic Tac Toe game!"
+    puts "|_#{@board[0]}_|_#{@board[1]}_|_#{@board[2]}_|\n|_#{@board[3]}_|_#{@board[4]}_|_#{@board[5]}_|\n|_#{@board[6]}_|_#{@board[7]}_|_#{@board[8]}_|\n"
+
+    choose_players
+  end
+
+  def choose_players
+    puts "Are you playing against a friend? Or will you try your luck against me?"
+    puts "Choose '1' for human vs. human.", "Choose 2 for human vs. computer."
+    choice = gets.chomp!.to_i
+    unless choice == 1 || choice == 2
+      puts "Please make a valid selection. '1' for human vs. human, and '2' to try your luck against me."
+      choose_players
+    end
+
+    choose_marker
+  end
+
+  #method to allow user to choose markers for both players.
+  def choose_marker
+    "Choose a unique marker for "
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+  def game_progression
+    puts "Please select your spot."
+    until game_is_over(@board) || tie(@board)
+      get_human_spot
+      if !game_is_over(@board) && !tie(@board)
+        eval_board
+      end
+      puts "|_#{@board[0]}_|_#{@board[1]}_|_#{@board[2]}_|\n|_#{@board[3]}_|_#{@board[4]}_|_#{@board[5]}_|\n|_#{@board[6]}_|_#{@board[7]}_|_#{@board[8]}_|\n"
+    end
+    puts "Game over"
+  end
+
+  def get_human_spot
+    spot = nil
+    until spot
+      spot = gets.chomp.to_i
+      if @board[spot] != "X" && @board[spot] != "O"
+        @board[spot] = @hum
+      else
+        spot = nil
+      end
+    end
+  end
+
+  def eval_board
+    spot = nil
+    until spot
+      if @board[4] == "4"
+        spot = 4
+        @board[spot] = @com
+      else
+        spot = get_best_move(@board, @com)
+        if @board[spot] != "X" && @board[spot] != "O"
+          @board[spot] = @com
+        else
+          spot = nil
+        end
+      end
+    end
+  end
+
+  def get_best_move(board, next_player, depth = 0, best_score = {})
+    available_spaces = []
+    best_move = nil
+    board.each do |s|
+      if s != "X" && s != "O"
+        available_spaces << s
+      end
+    end
+    available_spaces.each do |as|
+      board[as.to_i] = @com
+      if game_is_over(board)
+        best_move = as.to_i
+        board[as.to_i] = as
+        return best_move
+      else
+        board[as.to_i] = @hum
+        if game_is_over(board)
+          best_move = as.to_i
+          board[as.to_i] = as
+          return best_move
+        else
+          board[as.to_i] = as
+        end
+      end
+    end
+    if best_move
+      return best_move
+    else
+      n = rand(0..available_spaces.count)
+      return available_spaces[n].to_i
+    end
+  end
+
+  def game_is_over(b)
+
+    [b[0], b[1], b[2]].uniq.length == 1 ||
+    [b[3], b[4], b[5]].uniq.length == 1 ||
+    [b[6], b[7], b[8]].uniq.length == 1 ||
+    [b[0], b[3], b[6]].uniq.length == 1 ||
+    [b[1], b[4], b[7]].uniq.length == 1 ||
+    [b[2], b[5], b[8]].uniq.length == 1 ||
+    [b[0], b[4], b[8]].uniq.length == 1 ||
+    [b[2], b[4], b[6]].uniq.length == 1
+  end
+
+  def tie(b)
+    b.all? { |s| s == "X" || s == "O" }
+  end
+
+end
+
+game = Game.new
+game.start_game
